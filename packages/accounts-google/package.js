2a4b3b82898967d54f7880c58a2de52fdf7ab9b6
Jeff <jeffreyf@gmail.com>
2015-09-09 18:17:07 -0700

    Allowing use of most current external packages

--- packages/accounts-google/package.js
+++ packages/accounts-google/package.js
@@ -5,11 +5,11 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use(['underscore@1.0.3', 'random@1.0.3']);
-  api.use('accounts-base@1.2.0', ['client', 'server']);
+  api.use(['underscore', 'random']);
+  api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth@1.1.5', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
   api.use('emmerge:google@1.1.5', ['client', 'server']);
 
   api.addFiles('google_login_button.css', 'client');
