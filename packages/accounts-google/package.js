c7fcbb3a4b658fb30ec9e13404e13684edc3cfe4
aggregatejeff <jeffreyf@gmail.com>
2015-09-14 13:22:23 -0700

    Bring packages up to date

--- packages/accounts-google/package.js
+++ packages/accounts-google/package.js
@@ -1,39 +1,17 @@
-<<<<<<< HEAD
  Package.describe({
-+  name: 'emmerge:accounts-google',
+   name: 'emmerge:accounts-google',
    summary: "Login service for Google accounts",
-   version: "1.0.4"
+   version: "1.0.5-plugins.0"
  });
  
  Package.onUse(function(api) {
--  api.use(['underscore', 'random']);
--  api.use('accounts-base', ['client', 'server']);
-+  api.use(['underscore@1.0.3', 'random@1.0.3']);
-+  api.use('accounts-base@1.2.0', ['client', 'server']);
+   api.use(['underscore@1.0.4-plugins.0', 'random@1.0.4-plugins.0']);
+   api.use('accounts-base@1.2.1-plugins.0', ['client', 'server']);
    // Export Accounts (etc) to packages using this one.
-   api.imply('accounts-base', ['client', 'server']);
--  api.use('accounts-oauth', ['client', 'server']);
--  api.use('google', ['client', 'server']);
-+  api.use('accounts-oauth@1.1.5', ['client', 'server']);
-+  api.use('emmerge:google@1.1.5', ['client', 'server']);
+   api.imply('accounts-base@1.2.1-plugins.0', ['client', 'server']);
+   api.use('accounts-oauth@1.1.4-plugins.0', ['client', 'server']);
+   api.use('emmerge:google@11.1.6-plugins.0', ['client', 'server']);
  
    api.addFiles('google_login_button.css', 'client');
-=======
-Package.describe({
-  summary: "Login service for Google accounts",
-  version: "1.0.5-plugins.0"
+   api.addFiles("google.js");
 });
-
-Package.onUse(function(api) {
-  api.use(['underscore', 'random']);
-  api.use('accounts-base', ['client', 'server']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('google', ['client', 'server']);
-
-  api.addFiles('google_login_button.css', 'client');
-
-  api.addFiles("google.js");
-});
->>>>>>> meteor/devel
