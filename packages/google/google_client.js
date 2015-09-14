0dea335fad38405bff1b79d20cd940261858a343
aggregatejeff <jeffreyf@gmail.com>
2015-09-14 13:25:08 -0700

    Bring google client up to date

--- packages/google/google_client.js
+++ packages/google/google_client.js
@@ -26,30 +26,9 @@ Google.requestCredential = function (options, credentialRequestCompleteCallback)
   // always need this to get user id from google.
   var requiredScope = ['profile'];
   var scope = ['email'];
-
   if (options.requestPermissions)
     scope = options.requestPermissions;
   scope = _.union(scope, requiredScope);
-<<<<<<< HEAD
-  var flatScope = _.map(scope, encodeURIComponent).join('+');
- 
-  // https://developers.google.com/accounts/docs/OAuth2WebServer#formingtheurl
-  var accessType = options.requestOfflineToken ? 'offline' : 'online';
-
-  var loginStyle = OAuth._loginStyle('google', config, options);
-
-  var loginUrl =
-        'https://accounts.google.com/o/oauth2/auth' +
-        '?response_type=code' +
-        '&client_id=' + config.clientId +
-        '&scope=' + flatScope +
-        '&redirect_uri=' + OAuth._redirectUri('google', config) +
-        '&state=' + OAuth._stateParam(loginStyle, credentialToken) +
-        '&access_type=' + accessType;
-        
-  if (typeof options.prompt === 'string') {
-    loginUrl += '&prompt=' + options.prompt;
-=======
 
   var loginUrlParameters = {};
   if (config.loginUrlParameters){
@@ -71,21 +50,9 @@ Google.requestCredential = function (options, credentialRequestCompleteCallback)
   }
   if (options.prompt != null) {
     loginUrlParameters.prompt = options.prompt;
->>>>>>> meteor/devel
   } else if (options.forceApprovalPrompt) {
     loginUrlParameters.prompt = 'consent'
   }
-<<<<<<< HEAD
-  
-  // Use Google's domain-specific login page if we want to restrict creation to
-  // a particular email domain. (Don't use it if restrictCreationByEmailDomain
-  // is a function.) Note that all this does is change Google's UI ---
-  // accounts-base/accounts_server.js still checks server-side that the server
-  // has the proper email address after the OAuth conversation.
-  if (typeof Accounts._options.restrictCreationByEmailDomain === 'string') {
-    loginUrl += '&hd=' + encodeURIComponent(Accounts._options.restrictCreationByEmailDomain);
-  }
-=======
 
   var loginStyle = OAuth._loginStyle('google', config, options);
   // https://developers.google.com/accounts/docs/OAuth2WebServer#formingtheurl
@@ -100,7 +67,6 @@ Google.requestCredential = function (options, credentialRequestCompleteCallback)
     _.map(loginUrlParameters, function(value, param){
       return encodeURIComponent(param) + '=' + encodeURIComponent(value);
     }).join("&");
->>>>>>> meteor/devel
 
   OAuth.launchLogin({
     loginService: "google",
